fd92d022cebe1e52cb06efd58be84f1bf72e8600
wirawut22 <wirawut22@gmail.com>
2021-06-13 23:50:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -17,3 +17,9 @@ const nuxt = new Nuxt(config);
 
 // Add nuxt middleware
 app.use(nuxt.render);
+
+// Build on start
+new Builder(nuxt).build().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
